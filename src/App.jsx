fe2349d9077c79b81aaf34111116c7725daeaa61
Nichristandy <65210946+Nichristandy@users.noreply.github.com>
2024-04-04 14:40:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="font-bold text-6xl">Reserveat</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
