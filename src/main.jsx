84d5283a1fac7376b68dec3d9101ad2feb485bd1
Christopher Joshua <115674460+iWasKriss@users.noreply.github.com>
2024-04-18 21:33:57 +0700

    first push

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//Import Pages
+import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
+  {
+    path: "/home",
+    element:<HomePage />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
