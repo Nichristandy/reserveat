c16da77a37f2e54b86c128e1c5633d10a916d54b
Nichristandy <65210946+Nichristandy@users.noreply.github.com>
2024-05-13 11:06:37 +0700

    reservasi responsive

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -1,12 +1,20 @@
-const Table = ({ seatNumber }) => {
+const Table = ({ seatNumber, status }) => {
   return (
     <>
       {seatNumber == 6 ? (
-        <div className="col-span-2 bg-grey rounded-xl h-28 text-xl font-bold items-center justify-center flex">
+        <div
+          className={`col-span-2 ${
+            status == true ? "bg-grey" : "bg-red text-white"
+          } rounded-xl h-28 text-xl font-bold items-center justify-center flex`}
+        >
           6
         </div>
       ) : (
-        <div className="col-span-1 bg-grey rounded-xl text-xl font-bold items-center justify-center flex">
+        <div
+          className={`col-span-1 ${
+            status == true ? "bg-grey" : "bg-red text-white"
+          } rounded-xl text-xl font-bold items-center justify-center flex`}
+        >
           2
         </div>
       )}
