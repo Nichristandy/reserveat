6c6036d70bda94631d11cd6984263cc49dd4fbc0
Nichristandy <65210946+Nichristandy@users.noreply.github.com>
2024-05-15 15:05:42 +0700

    responsive reservasi user

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = ({ closeModal, tableName, seatNumber }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className=" min-w-[446px] bg-white rounded-xl">
+      <div className=" md:min-w-[446px] bg-white rounded-xl">
         <div className="flex-row flex items-center justify-between px-4 py-4 bg-red w-full rounded-t-xl text-white">
           <div className="flex flex-row gap-2">
             <img src=".././Logo.svg"></img>
--- src/pages/Reserve.jsx
+++ src/pages/Reserve.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Table from "../component/Table";
 import { MdOutlineArrowBack } from "react-icons/md";
 import Modal from "../component/Modal";
-
+import { NavLink } from "react-router-dom";
 const reserve = () => {
   const tables = [
     { tableName: "A", seatNumber: 6, status: true },
@@ -38,23 +38,27 @@ const reserve = () => {
 
   return (
     <>
-      <div className="min-h-screen px-[72px] pb-10">
+      <div className="min-h-screen px-5 md:px-[72px] pb-10">
         <div className="flex flex-row justify-between items-center">
-          <MdOutlineArrowBack size={32} />
+          <NavLink to={"/"}>
+            <MdOutlineArrowBack size={32} />
+          </NavLink>
           <div className="flex flex-row items-center gap-3 py-3">
             <img src="../Logo-merah.svg" alt="logo" />
             <span className="font-black text-[32px]">Reserveat</span>
           </div>
-          <button className="bg-red rounded-xl text-white uppercase px-2 py-1 font-bold text-2xl">
+          <button className="bg-red hidden md:flex rounded-xl text-white uppercase px-2 py-1 font-bold text-2xl">
             LOGIN
           </button>
         </div>
         <div className="pt-16 flex flex-col md:flex-row w-full">
           {/* sisi kiri */}
           <div className="flex flex-col w-full md:w-1/2">
-            <h1 className="font-black text-5xl">Lorem Ipsum</h1>
-            <span className="font-medium text-2xl mt-6">Choose your table</span>
-            <div className="flex flex-row gap-5 mt-5">
+            <h1 className="font-black text-3xl md:text-5xl">Rio Hamburger</h1>
+            <span className="font-medium text-2xl mt-3 md:mt-6">
+              Choose your table
+            </span>
+            <div className="flex flex-col md:flex-row gap-5 mt-2 md:mt-5">
               {" "}
               <div className="flex flex-row gap-5 items-center">
                 <span className="text-2xl font-bold">Hour:</span>{" "}
@@ -91,7 +95,7 @@ const reserve = () => {
               ></input>
             </div>
             {/* mapping table component */}
-            <div className="w-full grid grid-cols-5 gap-5 mt-10 pb-10">
+            <div className="w-full grid grid-cols-5 gap-5 mt-10 pb-5 md:pb-10">
               {tables.map((table) => (
                 <Table
                   key={table.tableName}
@@ -106,15 +110,15 @@ const reserve = () => {
           {/* menu sisi kanan */}
           <div className="w-full md:w-1/2 md:pl-8 flex flex-col gap-16">
             <div className="md:hidden flex"></div>
-            <div className="w-full min-h-[311px] relative image-back md:px-5 ">
+            <div className="w-full min-h-[146px] md:min-h-[311px] relative image-back md:px-5 ">
               <img src="../beef.png" className="z-0 absolute"></img>
-              <span className="absolute z-1 text-white font-black text-5xl top-3/4 md:top-full left-1 md:left-10">
+              <span className="absolute z-1 text-white font-black text-2xl md:text-5xl top-full md:top-full left-1 md:left-10">
                 See Menu
               </span>
             </div>
-            <div className="w-full md:mt-16 flex-col flex gap-8 md:px-5">
+            <div className="w-full mt-4 md:mt-16 flex-col flex gap-8 md:px-5">
               <span className="font-medium text-xl">Favorites</span>
-              <div className="w-full grid grid-cols-6 gap-5">
+              <div className="w-full grid grid-cols-4 md:grid-cols-6 gap-5">
                 <div className="col-span-2 min-h-[202px] bg-grey rounded-xl"></div>
                 <div className="col-span-2 min-h-[202px] bg-grey rounded-xl"></div>
                 <div className="col-span-2 min-h-[202px] bg-grey rounded-xl"></div>
