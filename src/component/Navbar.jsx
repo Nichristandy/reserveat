8520cdab08ea886ca36537000e48b4471a34effd
Christopher Joshua <115674460+iWasKriss@users.noreply.github.com>
2024-05-20 09:28:11 +0700

    link

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,31 @@
 import { NavLink } from "react-router-dom";
 import { MdOutlineArrowBack } from "react-icons/md";
 
-const Navbar = ({ to }) => {
+const Navbar = () => {
   return (
     <>
       {" "}
       <div className="w-full flex flex-row justify-between items-center">
-        <NavLink to={to}>
+        <NavLink to="/">
           <MdOutlineArrowBack size={32} />
         </NavLink>
         <div className="flex flex-row items-center gap-3 py-3">
           <img src="../Logo-merah.svg" alt="logo" />
           <span className="font-black text-[32px]">Reserveat</span>
         </div>
-        <button className="bg-red hidden md:flex rounded-xl text-white uppercase px-2 py-1 font-bold text-2xl">
-          LOGIN
-        </button>
+
+        <div className="flex gap-2">
+          <NavLink to="#">
+            <button className="bg-red hidden md:flex rounded-xl text-white uppercase px-2 py-1 font-bold text-2xl ">
+              History
+            </button>
+          </NavLink>
+          <NavLink to="#">
+            <button className="bg-red hidden md:flex rounded-xl text-white uppercase px-2 py-1 font-bold text-2xl">
+              LOGIN
+            </button>
+          </NavLink>
+        </div>
       </div>
     </>
   );
