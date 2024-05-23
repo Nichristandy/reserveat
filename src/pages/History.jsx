e3eeed0a39ddccf2703dca1d87e6d1149715ca3e
Christopher Joshua <115674460+iWasKriss@users.noreply.github.com>
2024-05-23 19:35:48 +0700

    t

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -3,58 +3,79 @@ import Navbar from "../component/Navbar";
 
 const History = () => {
   return (
-    <div>
+    <div className="history-container">
       <Navbar />
-      <div className="history-cont">
-        <section>
-          <div></div>
-        </section>
-        <section>
-          <h1>Order List</h1>
-          <nav>
-            <button>All</button>
-            <button>On Going</button>
-            <button>Done</button>
-            <button>Rescheduled</button>
-          </nav>
+      <section className="history-content">
+        <h1>Order List</h1>
+        <nav>
+          <button>All</button>
+          <button>On Going</button>
+          <button>Done</button>
+          <button>Rescheduled</button>
+        </nav>
+        <div className="history-orders">
+          <div className="history-order">
+            <span className="history-order-content">
+              <h2>Rio Burger</h2>
+              <span>Reserved</span>
+              <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
+            </span>
 
-          <div className="history-order">
-            <h2>Rio Burger</h2>
-            <span>Reserved</span>
-            <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
-            <button className="bg-orange-100">Rescheduled</button>
-            <button className="bg-rose-500">Cancel</button>
+            <div className="history-order-actions">
+              <button className="bg-orange-100">Rescheduled</button>
+              <button className="bg-rose-500">Cancel</button>
+            </div>
           </div>
           <div className="history-order">
-            <h2>Rio Burger</h2>
-            <span>Reserved</span>
-            <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
-            <button className="bg-orange-100">Rescheduled</button>
-            <button className="bg-rose-500">Cancel</button>
+            <span className="history-order-content">
+              <h2>Rio Burger</h2>
+              <span>Reserved</span>
+              <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
+            </span>
+
+            <div className="history-order-actions">
+              <button className="bg-orange-100">Rescheduled</button>
+              <button className="bg-rose-500">Cancel</button>
+            </div>
           </div>
           <div className="history-order">
-            <h2>Rio Burger</h2>
-            <span>Reserved</span>
-            <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
-            <button className="bg-orange-100">Rescheduled</button>
-            <button className="bg-rose-500">Cancel</button>
+            <span className="history-order-content">
+              <h2>Rio Burger</h2>
+              <span>Reserved</span>
+              <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
+            </span>
+
+            <div className="history-order-actions">
+              <button className="bg-orange-100">Rescheduled</button>
+              <button className="bg-rose-500">Cancel</button>
+            </div>
           </div>
           <div className="history-order">
-            <h2>Rio Burger</h2>
-            <span>Reserved</span>
-            <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
-            <button className="bg-orange-100">Rescheduled</button>
-            <button className="bg-rose-500">Cancel</button>
+            <span className="history-order-content">
+              <h2>Rio Burger</h2>
+              <span>Reserved</span>
+              <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
+            </span>
+
+            <div className="history-order-actions">
+              <button className="bg-orange-100">Rescheduled</button>
+              <button className="bg-rose-500">Cancel</button>
+            </div>
           </div>
           <div className="history-order">
-            <h2>Rio Burger</h2>
-            <span>Reserved</span>
-            <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
-            <button className="bg-orange-100">Rescheduled</button>
-            <button className="bg-rose-500">Cancel</button>
+            <span className="history-order-content">
+              <h2>Rio Burger</h2>
+              <span>Reserved</span>
+              <time dateTime="2024-05-26 12:00">12.00 - 13.30</time>
+            </span>
+
+            <div className="history-order-actions">
+              <button className="bg-orange-100">Rescheduled</button>
+              <button className="bg-rose-500">Cancel</button>
+            </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     </div>
   );
 };
